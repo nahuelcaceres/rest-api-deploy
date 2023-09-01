c9f121e723632372e2a36ca1a77fbddc18d9b0df
Nahuel <ncaceres@andreani.com>
2023-09-01 18:08:40 -0300

    add: first commit

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require("express"); // require --> comonJS
+const crypt = require("node:crypto");
+const cors = require("cors");
+const movies = require("./movies.json");
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:8080",
+        "http://localhost:1234",
+        "https://movies.com",
+        "https://midu.dev",
+      ];
+
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+app.disable("x-powered-by");
+
+// Todos los rescursos que sean MOVIES se identifican con /movies
+app.get("/movies", (req, res) => {
+  const { genre } = req.query;
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLocaleLowerCase())
+    );
+
+    return res.json(filteredMovies);
+  }
+
+  res.json(movies);
+});
+
+app.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+
+  const movie = movies.find((movie) => movie.id === id);
+  if (movie) return res.json(movie);
+
+  return res.status(404).json({ message: "Movie not found" });
+});
+
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    return res.status(422).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const newMovie = {
+    id: crypt.randomUUID(),
+    ...result.data,
+  };
+
+  // Esto no es REST, porque estamos guardando el estado en memoria
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
+});
+
+app.patch("/movies/:id", (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  if (result.error) {
+    return res.status(422).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+
+  movies[movieIndex] = updateMovie;
+
+  return res.json(updateMovie);
+});
+
+const PORT = process.env.PORT ?? 1234;
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`);
+});
